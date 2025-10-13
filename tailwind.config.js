4036c1a5c9e75dc5cdfcdab5b00f245a00449b9d
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2025-10-12 21:16:43 -0600

    style: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ export default {
       fontFamily: {
         sans: ['Roboto', 'sans-serif'],
       },
+      colors: {
+        'primary': {
+          50: '#ADE1FB',   // Lightest blue
+          100: '#ADE1FB',
+          200: '#ADE1FB',
+          300: '#2666CA',  // Medium blue
+          400: '#2666CA',
+          500: '#0F2573',  // Dark blue
+          600: '#0F2573',
+          700: '#041D56',  // Darker blue
+          800: '#041D56',
+          900: '#01082D',  // Darkest blue
+        },
+      },
     },
   },
   plugins: [],
